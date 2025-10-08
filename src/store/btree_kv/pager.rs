75d70135d681bb2d6f978527af24d0b5b4b9b1c1
Nidhi <nidhiraj9899@gmail.com>
2025-10-08 08:29:58 +0200

    refactor: make pager testable

--- src/store/btree_kv/pager.rs
+++ src/store/btree_kv/pager.rs
@@ -1,32 +1,31 @@
-use std::{collections::HashMap};
+use std::{collections::HashMap, sync::Arc};
+
+use mockall::automock;
 
 pub const PAGE_SIZE: usize = 8000; // 8kb.
 
 ///
 /// Defines the PageId of a Page of data in disk.
 ///
-#[derive(Eq, Hash, PartialEq, Copy, Clone)]
+#[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
 pub struct PageId {
     id: u8,
 }
 
-///
-/// Implements the methods to read from and write to files in disk.
-/// 
-pub struct Pager {
-    data: HashMap<PageId, [u8; PAGE_SIZE]>, // TODO: Currently an in-memory store. Update this to access File System.
+impl PageId {
+    ///
+    /// Creates a new instance of PageId.
+    /// 
+    pub fn new(id: u8) -> Self {
+        PageId { id }
+    }
 }
 
-impl Pager {
-    ///
-    /// Creates a new instance of Pager.
-    /// 
-    pub fn new() -> Self {
-        Pager { 
-            data: HashMap::new()
-        }
-    }
-
+///
+/// Implements the methods to read from and write to files in disk.
+///
+#[automock]
+pub trait PagerTrait {
     ///
     /// Fetches a page from disk and returns it.
     /// 
@@ -36,10 +35,7 @@ impl Pager {
     /// # Returns
     /// * `Option<&[u8; PAGE_SIZE]]>` containing the Page data.
     /// 
-    pub fn read_page(&self, id: PageId) -> Option<&[u8; PAGE_SIZE]> {
-        self.data.get(&id)
-    }
-
+    fn read_page(&self, id: PageId) -> Option<Arc<[u8; PAGE_SIZE]>>;
     ///
     /// Writes data to a Page.
     /// 
@@ -47,7 +43,34 @@ impl Pager {
     /// * `id`: Page ID of the Page.
     /// * `data`: Data that needs to be written.
     /// 
-    pub fn write_page(&mut self, id: PageId, data: &[u8]) {
+    fn write_page(&mut self, id: PageId, data: &[u8]);
+}
+ 
+pub struct Pager {
+    data: HashMap<PageId, [u8; PAGE_SIZE]>, // TODO: Currently an in-memory store. Update this to access File System.
+}
+
+impl Pager {
+    ///
+    /// Creates and returns an instance.
+    /// 
+    pub fn new() -> Self {
+        Pager { 
+            data: HashMap::new()
+        }
+    }
+}
+
+impl PagerTrait for Pager {
+    /// See [`PagerTrait::read_page`].
+    fn read_page(&self, id: PageId) -> Option<Arc<[u8; PAGE_SIZE]>> {
+        // Arc::new currently creates a copy of the data in the hashmap. This will be fixed once
+        // we move to storing data on disk.
+        self.data.get(&id).map(|p| Arc::new(*p))
+    }
+
+    /// See [`PagerTrait::write_page`].
+    fn write_page(&mut self, id: PageId, data: &[u8]) {
         if data.len() > PAGE_SIZE {
             // TODO: Handle this better.
             panic!("Page Limit Exceeded. Break data into smaller chunks.")
@@ -76,7 +99,7 @@ mod tests {
         let id = PageId{ id: 10 };
 
         pager.write_page(id, &page);
-        assert_eq!(&page, pager.read_page(id).unwrap());
+        assert_eq!(&page, pager.read_page(id).unwrap().as_ref());
     }
 
     #[test]
@@ -85,7 +108,7 @@ mod tests {
 
         let data = [10, 20, 30];
         let mut page = [0u8; PAGE_SIZE];
-        page[..data.len()].copy_from_slice(&data);;
+        page[..data.len()].copy_from_slice(&data);
 
         let id = PageId{ id: 10 };
 
@@ -96,7 +119,7 @@ mod tests {
         new_page[..new_data.len()].copy_from_slice(&new_data);
 
         pager.write_page(id, &new_page);
-        assert_eq!(&new_page, pager.read_page(id).unwrap());
+        assert_eq!(&new_page, pager.read_page(id).unwrap().as_ref());
     }
 
     #[test]
