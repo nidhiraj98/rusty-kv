135ae66097189790ca0e062ff8bb94d8e5e25a48
Nidhi Rajendra <nidhiraj9899@gmail.com>
2026-01-04 20:35:18 +0100

    refactor: Move BTreeRow constants to page.rs

--- src/store/btree_kv/constants/page_constants.rs
+++
@@ -1,7 +0,0 @@
-use std::mem::size_of;
-
-pub(crate) const KEY_SIZE_OFFSET: usize = 0;
-pub(crate) const KEY_SIZE_SIZE: usize = size_of::<u16>(); // 2 bytes
-pub(crate) const VALUE_SIZE_OFFSET: usize = KEY_SIZE_OFFSET + KEY_SIZE_SIZE;
-pub(crate) const VALUE_SIZE_SIZE: usize = size_of::<u16>(); // 2 bytes
-pub(crate) const ROW_HEADER_SIZE: usize = KEY_SIZE_SIZE + VALUE_SIZE_SIZE;
